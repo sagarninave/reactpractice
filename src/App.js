71e573eb66da2b237634ff6e80368a242624882a
sagarninave <saharninave1996@gmail.com>
2020-11-21 23:32:05 +0530

    crud functionality done

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ import ParentContext from './components/Context3';
 // import FontAwesome from './components/FontAwesome';
 import FunctionAsProps from './components/FunctionAsProps';
 import Portal from './components/Portal';
-import Create from './components/CRUD';
+import CRUD from './components/CRUD';
 
 function App() {
 
@@ -58,7 +58,7 @@ function App() {
 
       <div className="row" style={{margin:0, padding:0}}>
         <Router>
-          <div className="col-md-4" style={{height:"90vh", overflowY:"scroll", paddingBottom:"10px"}}>
+          <div className="col-md-3" style={{height:"90vh", overflowY:"scroll", paddingBottom:"10px"}}>
             <nav>
               <ul style={{listStyleType:"none", textAlign:"left", marginTop:"20px"}}>
                 <li> <Link to="/"> Dynamic Text Binding </Link> </li>
@@ -90,7 +90,7 @@ function App() {
                 <li> <Link to="/functional_form_validation"> Functional Form Validation </Link> </li>
                 <li> <Link to="/class_form"> Class Form </Link> </li>
                 <li> <Link to="/common_input_handle_form"> Common Input Field Handle Form </Link> </li>
-                <li> <Link to="/create_user"> CRUD </Link> </li>
+                <li> <Link to="/CRUD"> CRUD </Link> </li>
                 <li> <Link to="/functional_get_api_by_fetch"> Functional GET API By Fetch() </Link> </li>
                 <li> <Link to="/functional_get_api"> Functional GET API </Link> </li>
                 <li> <Link to="/functional_post_api"> Functional POST API  </Link> </li>
@@ -113,68 +113,68 @@ function App() {
               </ul>
             </nav>
           </div>
-          <div className="col-md-8" style={{marginTop:"20px"}}>
-          <ErrorBoundry>
-            <Switch>
-              <Route exact path="/"> {dynamicContent} </Route>
-              <Route path="/functional_component"> <FunctionComponent/> </Route>
-              <Route path="/class_component"> <ClassComponent/> </Route>
-              <Route path="/anonymous_function">  {AnonymousFunction("data title", "data description")} </Route>
-              <Route path="/functional_props"> <FunctionalProps name="sagar" last="ninave"/> </Route>
-              <Route path="/functional_child_props"> 
-                <FunctionChildProps name="vishal" last="">
-                  <span> user does not have last name </span> <br/>
-                  <span> user is VVIP </span> <br/>
-                </FunctionChildProps>
-              </Route>
-              <Route path="/functional_detailed_props"> <FunctionDetailedProps/> </Route>
-              <Route path="/class_props"> <ClassProps first_name="Sagar" last_name="Ninave"/> </Route>
-              <Route path="/event_handling"> <Event/> </Route>
-              <Route path="/functional_state"> <FunctionalState/> </Route>
-              <Route path="/class_state"> <ClassState/> </Route>
-              <Route path="/change_functional_state_data"> <ChangeFunctionalStateData/> </Route>
-              <Route path="/change_class_state_data"> <ChangeClassStateData/> </Route>
-              <Route path="/class_complete_lifecycle"> <CompleteLifeCycleClass/> </Route>
-              <Route path="/class_nested_lifecycle"> <NestedLifeCycleClass_ParentClass/> </Route>
-              <Route path="/class_component_will_unmount"> <ClassComponentWillUnmount/> </Route>
-              <Route path="/class_timer_lifecycle"> <ClassTimerLifecycle/> </Route>
-              <Route path="/functional_lifecycle"> <FunctionalLifeCycle/> </Route>
-              <Route path="/functional_fragment1"> <FunctionalFragment1/> </Route>
-              <Route path="/functional_fragment2"> <FunctionalFragment2/> </Route>
-              <Route path="/functional_fragment3"> <FunctionalFragment3/> </Route>
-              <Route path="/class_fragment"> <ClassFragment/> </Route>
-              <Route path="/simple_map_function"> <SimpleMapFunction/> </Route>
-              <Route path="/map_function_with_JSX"> <MapFunctionWithJSX/> </Route>
-              <Route path="/stylesheet"> <Stylesheet value={true} name={"sagar"}/> </Route>
-              <Route path="/functional_form"> <FunctionalForm/> </Route>
-              <Route path="/functional_form_validation"> <FunctionalFormValidation/> </Route>
-              <Route path="/class_form"> <ClassForm/> </Route>
-              <Route path="/common_input_handle_form"> <CommonInputHandleForm/> </Route>
-              <Route path="/create_user"> <Create/> </Route>
-              <Route path="/functional_get_api_by_fetch"> <FunctionalGetAPI_ByFetch/> </Route>
-              <Route path="/functional_get_api"> <FunctionalGetAPI/> </Route>
-              <Route path="/functional_post_api"> <FunctionalPostAPI/> </Route>
-              <Route path="/functional_post_api"> <FunctionalPostAPI/> </Route>
-              <Route path="/use_ref"> <UseRef/> </Route>
-              <Route path="/forword_ref"> <ForwordRefParent/> </Route>
-              <Route path="/class_pure_component"> <ClassPureComponent/> </Route>
-              <Route path="/functional_memo"> <FunctionalMemo/> </Route>
-              <Route path="/uncontrolled_component"> <UncontrolledComponent/> </Route>
-              <Route path="/lazy_loading"> <LazyLoading/> </Route>
-              <Route path="/props_type_cheking"> <PropsTypeCheking/> </Route>
-              <Route path="/custom_hook"> <CustomHook/> </Route>
-              <Route path="/simple_context"> <SimpleContext/> </Route>
-              <Route path="/nested_context"> <NestedContext/> </Route>
-              <Route path="/use_context_method"> <ParentContext/> </Route>
-              <Route path="/function_as_props"> <FunctionAsProps/> </Route>
-              <Route path="/portal"> <Portal/> </Route>
-              <Route path="/error_boundry"> <ErrorFunction/> </Route>
-              {/* <Route path="/font_awesome"> <FontAwesome/> </Route> */}
+          <div className="col-md-9" style={{marginTop:"20px"}}>
+            <ErrorBoundry>
+              <Switch>
+                <Route exact path="/"> {dynamicContent} </Route>
+                <Route path="/functional_component"> <FunctionComponent/> </Route>
+                <Route path="/class_component"> <ClassComponent/> </Route>
+                <Route path="/anonymous_function">  {AnonymousFunction("data title", "data description")} </Route>
+                <Route path="/functional_props"> <FunctionalProps name="sagar" last="ninave"/> </Route>
+                <Route path="/functional_child_props"> 
+                  <FunctionChildProps name="vishal" last="">
+                    <span> user does not have last name </span> <br/>
+                    <span> user is VVIP </span> <br/>
+                  </FunctionChildProps>
+                </Route>
+                <Route path="/functional_detailed_props"> <FunctionDetailedProps/> </Route>
+                <Route path="/class_props"> <ClassProps first_name="Sagar" last_name="Ninave"/> </Route>
+                <Route path="/event_handling"> <Event/> </Route>
+                <Route path="/functional_state"> <FunctionalState/> </Route>
+                <Route path="/class_state"> <ClassState/> </Route>
+                <Route path="/change_functional_state_data"> <ChangeFunctionalStateData/> </Route>
+                <Route path="/change_class_state_data"> <ChangeClassStateData/> </Route>
+                <Route path="/class_complete_lifecycle"> <CompleteLifeCycleClass/> </Route>
+                <Route path="/class_nested_lifecycle"> <NestedLifeCycleClass_ParentClass/> </Route>
+                <Route path="/class_component_will_unmount"> <ClassComponentWillUnmount/> </Route>
+                <Route path="/class_timer_lifecycle"> <ClassTimerLifecycle/> </Route>
+                <Route path="/functional_lifecycle"> <FunctionalLifeCycle/> </Route>
+                <Route path="/functional_fragment1"> <FunctionalFragment1/> </Route>
+                <Route path="/functional_fragment2"> <FunctionalFragment2/> </Route>
+                <Route path="/functional_fragment3"> <FunctionalFragment3/> </Route>
+                <Route path="/class_fragment"> <ClassFragment/> </Route>
+                <Route path="/simple_map_function"> <SimpleMapFunction/> </Route>
+                <Route path="/map_function_with_JSX"> <MapFunctionWithJSX/> </Route>
+                <Route path="/stylesheet"> <Stylesheet value={true} name={"sagar"}/> </Route>
+                <Route path="/functional_form"> <FunctionalForm/> </Route>
+                <Route path="/functional_form_validation"> <FunctionalFormValidation/> </Route>
+                <Route path="/class_form"> <ClassForm/> </Route>
+                <Route path="/common_input_handle_form"> <CommonInputHandleForm/> </Route>
+                <Route path="/CRUD"> <CRUD/> </Route>
+                <Route path="/functional_get_api_by_fetch"> <FunctionalGetAPI_ByFetch/> </Route>
+                <Route path="/functional_get_api"> <FunctionalGetAPI/> </Route>
+                <Route path="/functional_post_api"> <FunctionalPostAPI/> </Route>
+                <Route path="/functional_post_api"> <FunctionalPostAPI/> </Route>
+                <Route path="/use_ref"> <UseRef/> </Route>
+                <Route path="/forword_ref"> <ForwordRefParent/> </Route>
+                <Route path="/class_pure_component"> <ClassPureComponent/> </Route>
+                <Route path="/functional_memo"> <FunctionalMemo/> </Route>
+                <Route path="/uncontrolled_component"> <UncontrolledComponent/> </Route>
+                <Route path="/lazy_loading"> <LazyLoading/> </Route>
+                <Route path="/props_type_cheking"> <PropsTypeCheking/> </Route>
+                <Route path="/custom_hook"> <CustomHook/> </Route>
+                <Route path="/simple_context"> <SimpleContext/> </Route>
+                <Route path="/nested_context"> <NestedContext/> </Route>
+                <Route path="/use_context_method"> <ParentContext/> </Route>
+                <Route path="/function_as_props"> <FunctionAsProps/> </Route>
+                <Route path="/portal"> <Portal/> </Route>
+                <Route path="/error_boundry"> <ErrorFunction/> </Route>
+                {/* <Route path="/font_awesome"> <FontAwesome/> </Route> */}
 
-              <Route path="/404"> <PageNotFound/> </Route>
-              <Redirect to="/404"/>
+                <Route path="/404"> <PageNotFound/> </Route>
+                <Redirect to="/404"/>
 
-            </Switch>
+              </Switch>
             </ErrorBoundry>
           </div>
         </Router>
--- src/components/CRUD.js
+++ src/components/CRUD.js
@@ -1,7 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons';
+
 const axios = require('axios');
 
-function Create(){
+function CRUD(){
+
+  const myStyle = {
+    fontSize:"20px", 
+    marginTop:"10px", 
+    marginLeft:"30px",
+  };
 
   // const [first_name, set_first_name] = useState();
   // const [last_name, set_last_name] = useState();
@@ -10,16 +19,48 @@ function Create(){
   // const [dob, set_dob] = useState();
   // const [gender, set_gender] = useState();
   // const [password, set_password] = useState();
+
   const [value, setValue] = useState({
     first_name:"", 
     last_name:"", 
     email:"", 
     mobile:"", 
     dob:"", 
-    gender:"", 
-    password:""
+    gender:""
   });
-  const [submitResponse, set_submitResponse] = useState();
+
+  const [editUserValue, setEditUserValue] = useState({
+    first_name:"", 
+    last_name:"", 
+    email:"", 
+    mobile:"", 
+    dob:"", 
+    gender:""
+  });
+
+  const [allUsers, set_allUsers] = useState();
+  const [isShowUser, set_isShowUser] = useState(true);
+  const [isCreateUser, set_isCreateUser] = useState(false);
+  const [isEditUser, set_isEditUser] = useState(false);
+  const [editUserId, set_editUserId] = useState();
+
+  useEffect(() => {
+    getUsers();
+  }, [])
+
+  const getUsers = () => {
+    try{
+      axios({
+        method: 'get',
+        url: 'http://localhost:3030/users',
+      }).then(response => {
+        set_allUsers(response.data);
+      })    
+    }
+    catch(e){
+      console.log(e);
+    }
+  }
 
   const handleInput = (e) => {
     if(e.target.name === "first_name"){
@@ -40,24 +81,113 @@ function Create(){
     else if(e.target.name === "dob"){
       setValue({...value, dob: e.target.value});
     }    
-    else if(e.target.name === "password"){
-      setValue({...value, password: e.target.value});
-    }
     else{ }
   }
 
   function submitForm(e){
     e.preventDefault();
-    console.log("Form Value : ", value);
     try{
       axios({
         method: 'post',
-        url: 'http://localhost:3030/user/',
+        url: 'http://localhost:3030/users',
         data: value,
-        headers : {"Access-Control-Allow-Origin": "*"}
       }).then(response => {
-        console.log("Response : ", response);
-        set_submitResponse(response);
+        set_isCreateUser(false);
+        set_isShowUser(true);
+        getUsers();
+      })    
+    }
+    catch(e){
+      console.log(e);
+    }
+  }
+
+  const createUserView = () => {
+    set_isShowUser(false);
+    set_isCreateUser(true);
+    set_isEditUser(false);
+  }
+
+  const showUserView = () => {
+    set_isShowUser(true);
+    set_isCreateUser(false);
+    set_isEditUser(false);
+  }
+
+  const editUserView = (userID) => {
+    set_isShowUser(false);
+    set_isCreateUser(false);
+    set_isEditUser(true);
+    try{
+      axios({
+        method: 'get',
+        url: 'http://localhost:3030/users/'+userID,
+      }).then(response => {
+        let user = response.data;
+        set_editUserId(user.id);
+        setEditUserValue({
+          first_name:user.first_name,
+          last_name:user.last_name,
+          email:user.email,
+          mobile:user.mobile,
+          dob:user.dob,
+          gender:user.gender
+        });
+      })    
+    }
+    catch(e){
+      console.log(e);
+    }
+  }
+
+  const handleEditInput = (e) => {
+    if(e.target.name === "first_name"){
+      setEditUserValue({...editUserValue, first_name: e.target.value});
+    }
+    else if(e.target.name === "last_name"){
+      setEditUserValue({...editUserValue, last_name: e.target.value});
+    }    
+    else if(e.target.name === "email"){
+      setEditUserValue({...editUserValue, email: e.target.value});
+    }    
+    else if(e.target.name === "mobile"){
+      setEditUserValue({...editUserValue, mobile: e.target.value});
+    }    
+    else if(e.target.name === "gender"){
+      setEditUserValue({...editUserValue, gender: e.target.value});
+    }    
+    else if(e.target.name === "dob"){
+      setEditUserValue({...editUserValue, dob: e.target.value});
+    }    
+    else{ }
+  }
+
+  const userEditForm = (e) => {
+    e.preventDefault();
+    try{
+      axios({
+        method: 'put',
+        url: 'http://localhost:3030/users/'+editUserId,
+        data: editUserValue,
+      }).then(response => {
+        getUsers();
+        set_isEditUser(false);
+        set_isShowUser(true);
+      })    
+    }
+    catch(e){
+      console.log(e);
+    }
+
+  }
+
+  const deleteUser = (userID) => {
+    try{
+      axios({
+        method: 'delete',
+        url: 'http://localhost:3030/users/'+userID,
+      }).then(response => {
+        getUsers();
       })    
     }
     catch(e){
@@ -68,31 +198,90 @@ function Create(){
   return(
     <div className="container">
       <strong className="heading"> CRUD Operation </strong>
-      <div className="row">
-        <div className="col-md-8 offset-md-2">
-          {
-            submitResponse && 
-            <p>
-              <span> Record save successfully </span>
-              {submitResponse}
-            </p>
-          }
-          <form className="form-group" onSubmit={submitForm}>
-            <br/><input type="text" name="first_name" placeholder="First Name" onChange={handleInput} className="form-input"></input>
-            <br/><input type="text" name="last_name" placeholder="Last Name" onChange={handleInput} className="form-input"></input>
-            <br/><input type="email" name="email" placeholder="Email" onChange={handleInput} className="form-input"></input>
-            <br/><input type="text" name="mobile" placeholder="Mobile" onChange={handleInput} className="form-input"></input>
-            <br/><input type="date" name="dob" placeholder="dob" onChange={handleInput} className="form-input"></input>
-            <br/><select name="gender" onChange={handleInput}>
-              <option> Male </option>
-              <option> Female </option>
-            </select>
-            <br/><input type="password" name="password" placeholder="Password" onChange={handleInput} className="form-input"></input>
-            <br/><button> Create Account </button>
-          </form>
-        </div>
+      <div>
+        <button onClick={showUserView}> Show Users</button>
+        <button onClick={createUserView}> Create User</button>
       </div>
+      { isShowUser && 
+        <div className="row" style={{margin:"0px", padding:"0px"}}>
+          <div className="col-md-12" style={{margin:"0px", padding:"0px"}}>
+            <div className="row" style={{margin:"0px", padding:"0px"}}>
+              <div className="col-md-1" style={{margin:"0px", padding:"0px"}}> ID </div>
+              <div className="col-md-1" style={{margin:"0px", padding:"0px"}}> Fname </div>
+              <div className="col-md-1" style={{margin:"0px", padding:"0px"}}> Lname </div>
+              <div className="col-md-2" style={{margin:"0px", padding:"0px"}}> Email </div>
+              <div className="col-md-2" style={{margin:"0px", padding:"0px"}}> Mobile </div>
+              <div className="col-md-2" style={{margin:"0px", padding:"0px"}}> DoB </div>
+              <div className="col-md-1" style={{margin:"0px", padding:"0px"}}> Gender </div>
+              <div className="col-md-2" style={{margin:"0px", padding:"0px"}}> Action </div>
+            </div>
+            { allUsers ?
+              Object.values(allUsers).map((user, index) => {
+                return(
+                  <div className="row" key={index}>
+                    <div className="col-md-1" style={{margin:"0px", padding:"0px"}}> {user.id} </div>
+                    <div className="col-md-1" style={{margin:"0px", padding:"0px"}}> {user.first_name} </div>
+                    <div className="col-md-1" style={{margin:"0px", padding:"0px"}}> {user.last_name} </div>
+                    <div className="col-md-2" style={{margin:"0px", padding:"0px"}}> {user.email} </div>
+                    <div className="col-md-2" style={{margin:"0px", padding:"0px"}}> {user.mobile} </div>
+                    <div className="col-md-2" style={{margin:"0px", padding:"0px"}}> {user.dob} </div>
+                    <div className="col-md-1" style={{margin:"0px", padding:"0px"}}> {user.gender} </div>
+                    <div className="col-md-2" style={{margin:"0px", padding:"0px"}}>             
+                      <FontAwesomeIcon style={myStyle, {color:"green", marginRight:"10px"}} onClick={()=>editUserView(user.id)} icon={faEdit}/> 
+                      <FontAwesomeIcon style={myStyle, {color:"red"}} icon={faTrash} onClick={()=>deleteUser(user.id)}/> 
+                    </div>
+                  </div>
+                )
+              }) : <p style={{marginTop:"100px", color:"blue", fontWeight:"bolder", fontSize:"30px"}}> Please Wait ... !</p>
+            }
+          </div>
+        </div> 
+      }
+      
+      {
+        isCreateUser &&
+        <div className="row">
+          <div className="col-md-8 offset-md-2">
+            <h3 style={{marginTop:'20px', marginBop: '20px'}}> Create User </h3>
+            <form className="form-group" onSubmit={submitForm}>
+              <br/><input type="text" name="first_name" placeholder="First Name" onChange={handleInput} className="form-input"></input>
+              <br/><input type="text" name="last_name" placeholder="Last Name" onChange={handleInput} className="form-input"></input>
+              <br/><input type="email" name="email" placeholder="Email" onChange={handleInput} className="form-input"></input>
+              <br/><input type="text" name="mobile" placeholder="Mobile" onChange={handleInput} className="form-input"></input>
+              <br/><input type="date" name="dob" placeholder="dob" onChange={handleInput} className="form-input"></input>
+              <br/><select name="gender" onChange={handleInput}>
+                <option> Gender </option>
+                <option value="male"> Male </option>
+                <option value="female"> Female </option>
+              </select>
+              <br/><button> Create </button>
+            </form>
+          </div> 
+        </div>
+      }
+
+      {
+        isEditUser &&
+        <div className="row">
+          <div className="col-md-8 offset-md-2">
+            <h3 style={{marginTop:'20px', marginBottom: '0'}}> Edit User </h3>
+            <form className="form-group" onSubmit={userEditForm}>
+              <br/><input type="text" name="first_name" placeholder="First Name" value={editUserValue.first_name} onChange={handleEditInput} className="form-input"></input>
+              <br/><input type="text" name="last_name" placeholder="Last Name" value={editUserValue.last_name} onChange={handleEditInput} className="form-input"></input>
+              <br/><input type="email" name="email" placeholder="Email" value={editUserValue.email} onChange={handleEditInput} className="form-input"></input>
+              <br/><input type="text" name="mobile" placeholder="Mobile" value={editUserValue.mobile} onChange={handleEditInput} className="form-input"></input>
+              <br/><input type="date" name="dob" placeholder="dob" value={editUserValue.dob} onChange={handleEditInput} className="form-input"></input>
+              <br/><select name="gender" value={editUserValue.gender} onChange={handleEditInput}>
+                <option> Gender </option>
+                <option value="male"> Male </option>
+                <option value="female"> Female </option>
+              </select>
+              <br/><button> Edit </button>
+            </form>
+          </div> 
+        </div>
+      }
     </div>
   )
 }
-export default Create;
+export default CRUD;
